014add8f5d108d343f14fd6434e36ce039a847bf
Stav Corcos <poonchys@gmail.com>
2022-01-24 17:36:03 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/LabsforHashicorp/s3-bucket/aws"
+  version = "2.2.0"
+  
   bucket = "my-s3-bucket"
   bucket_prefix = "stavcorcos"
   acl    = "private"
